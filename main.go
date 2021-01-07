927a43dcc12e205b86135fc418176ccf6fc8b8bd
Nikolay Chumakov <nklchumakov@gmail.com>
2021-01-07 17:05:01 +0300

    Heroku update

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
-package backend
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello World")
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	http.HandleFunc("/", hello)
+	log.Print("Listening on :" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
